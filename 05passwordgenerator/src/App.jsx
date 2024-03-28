c68da65908360d0238eb025bac699bfc1ff981dc
Harshit <panjwaniharshit02@gmail.com>
2024-03-28 14:09:06 +0530

    Password Generator Project Completed

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -1,10 +1,13 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 
 function App() {
   const [length, setLength] = useState(8);
   const [numAllowed, setNumAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
+
+  //useRef hook
+  const passwordRef = useRef(null);
 
   const passwordGenerator = useCallback(() => {
     let pass = "";
@@ -21,16 +24,26 @@ function App() {
 
   }, [length, numAllowed, charAllowed, setPassword])
 
+  const copyPassToClipboard =()=>{
+    passwordRef.current?.select();
+    window.navigator.clipboard.writeText(password)
+  }
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numAllowed, charAllowed, passwordGenerator])
+
+
   return (
     <>
 
       <div className='w-full  max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800'>
         <h1 className='text-white text-center my-3'>Password Generator</h1>
         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
-          <input type='text' value={password} className='outline-none w-full py-1 px-3 ' readOnly placeholder='Pass(ssshhhhhh)word' >
+          <input type='text' value={password} className='outline-none w-full py-1 px-3 ' readOnly placeholder='Pass(ssshhhhhh)word' ref={passwordRef} >
           </input>
 
-          <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>
+          <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0' onClick={copyPassToClipboard}>
             copy
           </button>
         </div>
