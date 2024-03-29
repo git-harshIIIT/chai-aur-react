6495f677135d330b21b5f186db5d1a22f964a36e
Harshit <panjwaniharshit02@gmail.com>
2024-03-29 13:53:39 +0530

    updated with changes

--- 06currencychanger/src/components/InputBox.jsx
+++ 06currencychanger/src/components/InputBox.jsx
@@ -43,7 +43,7 @@ function InputBox({
                     {currencyOptions.map((currency) => {
                         return (
                             <option key={currency} value={currency}>
-                                usd
+                                {currency}
                             </option>
                         );
                     })}
